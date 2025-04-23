d427df57e4e8ec21afcb3ffaf22f91869e722e1a
ikhsanmaaa <ikhsanmaaa@gmail.com>
2025-04-23 19:52:29 +0700

    changed db

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { DATABASE_URL } from "./env";
 
 async function db() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mern-project", {
-      dbName: "mern-project",
+    await mongoose.connect(DATABASE_URL, {
+      dbName: "db-mern",
     });
     return Promise.resolve("database connected!");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
